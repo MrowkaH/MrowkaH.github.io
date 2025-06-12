7f43331c6d2a7b6a24669b2e518ff9988424efdf
Kacper Kulik <145699227+MrowkaH@users.noreply.github.com>
2025-06-12 21:02:05 +0200

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,26 @@ function toggleLanguage() {
             element.style.display = 'none';
         }
     });
-    
+    <script>
+  var modal = document.getElementById('myModal');
+  var img = document.getElementById('myImg');
+  var modalImg = document.getElementById('img01');
+  var span = document.getElementsByClassName('close')[0];
+
+  img.onclick = function() {
+    modal.style.display = 'block';
+    modalImg.src = this.src;
+  }
+  span.onclick = function() {
+    modal.style.display = 'none';
+  }
+  modal.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = 'none';
+    }
+  }
+</script>
+
     // Update language toggle button text
     const langButton = document.getElementById('lang-text');
     if (langButton) {
